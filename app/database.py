e540334625bc57f3779d4e9ab06310d363fb4485
anhvan3sp <anhvan3sp@gmail.com>
2026-02-07 17:32:08 +0700

    Add dotenv support for loading DATABASE_URL
    Load environment variables from a .env file.

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -8,3 +11,5 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
