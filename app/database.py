42f8664f6595fe09a0736300839359ce71f7375b
anhvan3sp <anhvan3sp@gmail.com>
2026-02-08 10:52:57 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 import os
+from urllib.parse import quote_plus
+
+
+raw_password = os.getenv("DB_PASSWORD")
+password = quote_plus(raw_password)
+
+DATABASE_URL = (
+    f"mysql+pymysql://avnadmin:{password}"
+    f"@mysql-1f784f93-anhvan2sp-e73a.aivencloud.com:17593/mymusic"
+)
 
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL chưa được cấu hình")
 
