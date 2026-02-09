42c15443a2d14fe585ba144e2ce2a674b0447dc2
anhvan3sp <anhvan3sp@gmail.com>
2026-02-09 07:15:43 +0700

    Clean up database.py by removing unused lines
    Removed unnecessary blank lines and commented out code for DATABASE_URL validation.

--- app/database.py
+++ app/database.py
@@ -2,20 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-
-
-
 DATABASE_URL = (
    
     f"mysql+pymysql://avnadmin:AVNS_zZUQx5y6vFp3QLX0T3_@mysql-1f784f93-anhvan2sp-e73a.a.aivencloud.com:17593/mymusic"
 )
-
-
-
-
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL chưa được cấu hình")
 
 engine = create_engine(
     DATABASE_URL,
